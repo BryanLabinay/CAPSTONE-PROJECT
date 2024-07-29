c276542ea36efe70e1e5834a25eaca549fd391e6
Arnold <junfernando727@gmail.com>
2024-07-29 22:41:34 +0800

    blade file for medical certificate pdf
    note: this is for pdf file

---
+++ resources/views/Admin/medicalcertificate-pdf.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Medical Certificate</title>
+</head>
+
+<body>
+    <h1>Medical Certificate</h1>
+    <p>This is to certify that <strong>{{ $patient_name }}</strong> has been examined and treated at our facility.</p>
+</body>
+
+</html>
