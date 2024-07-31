b4cc2c56d58a10bc5f4b32a974466cc296075d9c
Mark Bryan Labinay <smokefacebook02@gmail.com>
2024-07-31 23:34:40 +0800

    modify the pdf design

--- resources/views/Admin/medicalcertificate-pdf.blade.php
+++ resources/views/Admin/medicalcertificate-pdf.blade.php
@@ -1,30 +1,58 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-    <title>Medical Certificate</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>DR.MENDOZA MEDICAL CERTIFICATE</title>
+    <link rel="shortcut icon" href="Image/logo/mendoza.png" type="image/x-icon">
+    <link rel="stylesheet" href="Css/med-certif.css">
 </head>
 
 <body>
-    <h1>Medical Certificate</h1>
-    <p>This is to certify that <strong>{{ $patient_name }}</strong> has been examined and treated at our facility.</p>
+    <header>
+        <div class="img">
+            <img src="Image/logo/mendoza.png">
+        </div>
+        <h2>DR. MENDOZA MULTI-SPECIALIST CLINIC</h2>
+        <p class="address">
+            Magsaysay Corner St. Minanga Aparri Cagayan <br>
+            CEL# 0917 574 4643
+        </p>
+        <p class="title"></p>
+    </header>
+    <footer>
+        <div class="div2">Footer Content Here</div>
+        <div>Page <span class="pagenum"></span></div>
+    </footer>
+
+
+    <h1 class="med-certif">MEDICAL CERTIFICATE</h1>
+
+    <p class="patient">This is to certify that <strong>{{ $patient_name }}</strong> from
+        <strong>{{ $address }}</strong> has been
+        examined and treated at our facility.
+    </p>
 
     <h2>Medical Information</h2>
     <ul>
-        <li><strong>Heart:</strong> {{ $heart }}</li>
-        <li><strong>Lung:</strong> {{ $lung }}</li>
-        <li><strong>HEENT:</strong> {{ $heent }}</li>
-        <li><strong>Abdomen:</strong> {{ $abdomen }}</li>
-        <li><strong>Extremeties:</strong> {{ $extremeties }}</li>
-        <li><strong>Integumentary:</strong> {{ $intergumentary }}</li>
+        <li><strong>Heart:</strong> {{ $heart ?? 'NO' }}</li>
+        <li><strong>Lung:</strong> {{ $lung ?? 'NO' }}</li>
+        <li><strong>HEENT:</strong> {{ $heent ?? 'NO' }}</li>
+        <li><strong>Abdomen:</strong> {{ $abdomen ?? 'NO' }}</li>
+        <li><strong>Extremeties:</strong> {{ $extremeties ?? 'NO' }}</li>
+        <li><strong>Integumentary:</strong> {{ $intergumentary ?? 'NO' }}</li>
     </ul>
 
     <h2>Vital Signs</h2>
     <ul>
-        <li><strong>Blood Pressure (BP):</strong> {{ $bp }}</li>
-        <li><strong>Cardiac Rate (CR):</strong> {{ $cr }}</li>
-        <li><strong>Weight:</strong> {{ $weight }}</li>
-        <li><strong>Height:</strong> {{ $height }}</li>
+
+        <li><strong>Blood Pressure (BP):</strong> {{ $bp ?? 'NO' }}</li>
+        <li><strong>Cardiac Rate (CR):</strong> {{ $cr ?? 'NO' }}</li>
+        <li><strong>Weight:</strong> {{ $weight ?? 'NO' }}</li>
+        <li><strong>Height:</strong> {{ $height ?? 'NO' }}</li>
+        <li><strong>Date:</strong> {{ $date ?? 'NO' }}</li>
     </ul>
 </body>
 
