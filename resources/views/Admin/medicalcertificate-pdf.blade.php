e8391e41c6d239642bbc1aec19644a44836e296c
Mark Bryan Labinay <smokefacebook02@gmail.com>
2024-08-01 11:47:07 +0800

    add more information about medical certificate

--- resources/views/Admin/medicalcertificate-pdf.blade.php
+++ resources/views/Admin/medicalcertificate-pdf.blade.php
@@ -11,6 +11,7 @@
 </head>
 
 <body>
+    {{-- Header --}}
     <header>
         <div class="img">
             <img src="Image/logo/mendoza.png">
@@ -22,38 +23,58 @@
         </p>
         <p class="title"></p>
     </header>
+
+    {{-- Footer --}}
     <footer>
-        <div class="div2">Footer Content Here</div>
-        <div>Page <span class="pagenum"></span></div>
+        <div class="doc-title">
+            <p class="doc-name">Dr. Mark Bryan Labinay</p>
+            <ul class="ul">
+                <li class="li">MEDICAL OFFICER III</li>
+                <li class="li">APARRI PROVINCIAL HOSPITAL</li>
+            </ul>
+        </div>
     </footer>
 
+    {{-- Content --}}
+    <img src="Image/logo/mendoza.png" class="body-img">
 
     <h1 class="med-certif">MEDICAL CERTIFICATE</h1>
-
+    {{-- {{ $date ?? 'NO' }} --}}
+    <h5 class="date">August 01, 2024</h5>
     <p class="patient">This is to certify that <strong>{{ $patient_name }}</strong> from
-        <strong>{{ $address }}</strong> has been
-        examined and treated at our facility.
+        <strong>{{ $address }}</strong> was seen
+        examined in this clinic.
     </p>
 
-    <h2>Medical Information</h2>
+    <h3 class="phy-exam">PHYSICAL EXAMINATIONS:</h3>
     <ul>
-        <li><strong>Heart:</strong> {{ $heart ?? 'NO' }}</li>
-        <li><strong>Lung:</strong> {{ $lung ?? 'NO' }}</li>
-        <li><strong>HEENT:</strong> {{ $heent ?? 'NO' }}</li>
-        <li><strong>Abdomen:</strong> {{ $abdomen ?? 'NO' }}</li>
-        <li><strong>Extremeties:</strong> {{ $extremeties ?? 'NO' }}</li>
-        <li><strong>Integumentary:</strong> {{ $intergumentary ?? 'NO' }}</li>
+        <li>Heart: {{ $heart ?? 'NO' }}</li>
+        <li>Chest/Lung: {{ $lung ?? 'NO' }}</li>
+        <li>HEENT: {{ $heent ?? 'NO' }}</li>
+        <li>Abdomen: {{ $abdomen ?? 'NO' }}</li>
+        <li>Extremeties: {{ $extremeties ?? 'NO' }}</li>
+        <li>Integumentary: {{ $intergumentary ?? 'NO' }}</li>
     </ul>
 
-    <h2>Vital Signs</h2>
+    <h3 class="phy-exam">VITAL SIGNS:</h3>
     <ul>
-
-        <li><strong>Blood Pressure (BP):</strong> {{ $bp ?? 'NO' }}</li>
-        <li><strong>Cardiac Rate (CR):</strong> {{ $cr ?? 'NO' }}</li>
-        <li><strong>Weight:</strong> {{ $weight ?? 'NO' }}</li>
-        <li><strong>Height:</strong> {{ $height ?? 'NO' }}</li>
-        <li><strong>Date:</strong> {{ $date ?? 'NO' }}</li>
+        <li><i>Blood Pressure (BP): {{ $bp ?? 'NO' }}mmHg</i></li>
+        <li><i>Cardiac Rate (CR): {{ $cr ?? 'NO' }}</i></li>
+        <li><i>Weight: {{ $weight ?? 'NO' }}KG</i></li>
+        <li><i>Height: {{ $height ?? 'NO' }}CM </i></li>
     </ul>
+    <h3 class="assessment"><i>ASSESSMENT: ESSENTIALLY NORMAL AT THE TIME OF EXAMINATION</i></h3>
+    <h3 class="phy-exam">REMARKS/DIAGNOSIS: FIT TO WORK CLASS A</h3>
+    <ul>
+        <li>CLASS A: Physically fit for all types of work. No physical defects or disease noted</li>
+        <li>CLASS B: Physically fit for all types of work. Has minor defect/ailment.</li>
+        <li>CLASS C: Employable but owing to certain impairments or conditions. Recommending follow up/evaluation. </li>
+    </ul>
+
+    <h4><i>Note: Please see attached files</i></h4>
+    <p>This is Issued Upon His/Her Request For Job Employment Purposes.</p>
+    <p class="title"></p>
+    </div>
 </body>
 
 </html>
