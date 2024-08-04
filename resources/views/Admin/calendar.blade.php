abe09bed0b069d233da04eeaefbccffd661f5cc7
Mark Bryan Labinay <smokefacebook02@gmail.com>
2024-08-04 14:15:11 +0800

    adjust some font weight

--- resources/views/Admin/calendar.blade.php
+++ resources/views/Admin/calendar.blade.php
@@ -105,8 +105,8 @@
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="appointmentModalLabel">Appointments on <span
-                        id="modalDate">{{ $formattedDate }}</span></h5>
+                <h5 class="modal-title" id="appointmentModalLabel">Appointments on <span id="modalDate"
+                        class="fw-bold">{{ $formattedDate }}</span></h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
@@ -134,7 +134,7 @@
                 </table>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
             </div>
         </div>
     </div>
