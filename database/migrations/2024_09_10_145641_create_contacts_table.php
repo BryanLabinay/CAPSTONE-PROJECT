5635c03df27fda89e821b14b1b20ccaf663a898b
Mark Bryan Labinay <smokefacebook02@gmail.com>
2024-09-10 23:28:17 +0800

    remove useless migrations

--- database/migrations/2024_09_10_145641_create_contacts_table.php
+++ database/migrations/2024_09_10_145641_create_contacts_table.php
@@ -11,8 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('bryans', function (Blueprint $table) {
+        Schema::create('contacts', function (Blueprint $table) {
             $table->id();
+            $table->string('cpnumber');
+            $table->string('email');
             $table->timestamps();
         });
     }
@@ -22,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('bryans');
+        Schema::dropIfExists('contacts');
     }
 };
