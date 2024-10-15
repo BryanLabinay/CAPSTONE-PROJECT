044025cc1f40cbf8a57823096f309bb5f4d0f727
Arnold <junfernando727@gmail.com>
2024-10-15 19:43:37 +0800

    normalized the user table and add seeder to have sa sample data

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('fname');
+            $table->string('mname')->nullable();
+            $table->string('lname');
+            $table->string('suffix')->nullable();
             $table->string('email')->unique();
             $table->string('usertype')->default('user');
             $table->string('image')->nullable();
