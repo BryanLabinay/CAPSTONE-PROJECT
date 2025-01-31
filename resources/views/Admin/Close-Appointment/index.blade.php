d2825a8d8c4ef2faf751925f213b043f691db4c7
Mark Bryan Labinay <smokefacebook02@gmail.com>
2025-01-31 20:20:59 +0800

    add success toast notification for appointment closure confirmation

--- resources/views/Admin/Close-Appointment/index.blade.php
+++ resources/views/Admin/Close-Appointment/index.blade.php
@@ -69,6 +69,28 @@
 @section('content_header')
     <h5 class="fw-bolder" style="color: #343984;"><i class="fa-solid fa-caret-right me-2"></i>Close Appointment</h5>
     <hr class="mt-0 text-secondary">
+
+    @if (session('success'))
+        <script>
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                iconColor: 'white',
+                customClass: {
+                    popup: 'colored-toast',
+                },
+                showConfirmButton: false,
+                timer: 3000,
+                timerPr0ogressBar: true,
+            });
+            (async () => {
+                await Toast.fire({
+                    icon: 'success',
+                    title: 'Done Appointment'
+                })
+            })()
+        </script>
+    @endif
 @stop
 
 @section('content')
